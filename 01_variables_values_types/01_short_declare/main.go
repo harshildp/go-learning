72c17cc0b4447c7a2cbc82e018ecab9c7a624423
Harshil Patel <h.gibbs62@gmail.com>
2020-11-08 23:31:17 -0600

    Added some useful comments for later

--- 01_variables_values_types/01_short_declare/main.go
+++ 01_variables_values_types/01_short_declare/main.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
+// func main inside package main is the entry point to an application in Golang
 func main() {
 
 	// := declares and assigns a value to an identifier
+	// all declared variables must be used or else Go compiler
+	// will throw an unused error
 	x := 42
 	y := "Konnichiwa"
 	z := true
 
+	// Print outputs to the console and uses a variadic parameter ...interface{} 
+	// i.e. it can take N number of arguments of varying types as all types are
+	// of type interface{} at there base
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
--- 01_variables_values_types/02_var/main.go
+++ 01_variables_values_types/02_var/main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 )
 
-// Declares a variable with an identifer ex. "x" with a type 
+// Declares a variable with an identifier ex. "x" with a type 
 // Assigns the zero_value associated with the type
+// 0 for int, "" for String, false for bool, 0.0 for float
 var x int
 var y string
 var z bool
